4190389e597a9af339acca0dea3aebe0e1b2bf2b
yoonseo-323 <yc38436@gmail.com>
2026-06-17 20:27:52 +0900

    app.js: CORS 설정 변경

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,10 @@ app.use(cors({
     'https://greedy-grandpa-crabbing.ngrok-free.dev',
   ],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization','ngrok-skip-browser-warning'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 }));
 app.use(express.json());
 
